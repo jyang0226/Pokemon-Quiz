359fc9ed427ed67b25aca2aaeae32468df394306
Jaden Falbee <j.falbee04@gmail.com>
2023-09-16 21:43:37 -0400

    fixes img issue

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-let currentPokemonName = " ";
+let currentPokemonName = "";
 
 async function fetchRandomPokemon() {
     const randomId = Math.floor(Math.random() * 151) + 1;
     try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/${randomId}');
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
         const data = await response.json();
-
+        
         const pokemonImage = document.getElementById("pokemonImage");
         currentPokemonName = data.name;
         
         pokemonImage.src = data.sprites.front_default;
     } catch (error) {
-        console.log("Error catching Pokemon", error);
+        console.error("Error fetching Pokémon:", error);
     }
 }
 
 document.getElementById("submitGuess").addEventListener("click", function() {
-    const userGuess = document.getElementById("userGuess").ariaValueMax.toLowerCase();
+    const userGuess = document.getElementById("userGuess").value.toLowerCase();
     const result = document.getElementById("result");
-
+    
     if (userGuess === currentPokemonName) {
-        result.textContent = "You caught the Pokemon!";
+        result.textContent = "Pokemon Caught!";
         fetchRandomPokemon();
     } else {
-        result.textContent = "Pokemon fled :("
+        result.textContent = "Pokemon Fled...";
         fetchRandomPokemon();
     }
-})
+});
 
 fetchRandomPokemon();
